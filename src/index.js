414a41d143141139a94dd40939a3f03ae0fb2d51
Alejandro Mur <alejandro@mamutlove.com>
2025-03-29 16:25:08 +0100

    build: Add cors dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const { processSonification } = require("./controllers/sonificationController");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/api/sonificate", processSonification);
