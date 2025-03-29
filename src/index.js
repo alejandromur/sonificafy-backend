4d9fb22d0825070b8cc239644b2a4eb19ea00b9e
Alejandro Mur <alejandro@mamutlove.com>
2025-03-29 16:25:53 +0100

    refactor: modify sonification endpoint path

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/sonificate", processSonification);
+app.post("/api/sonification", processSonification);
 
 app.use(errorHandler);
 
