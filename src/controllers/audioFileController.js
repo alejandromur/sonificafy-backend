55ea644ecd2fd68b67aad2166fdb5374e7839370
Alejandro Mur <alejandro@mamutlove.com>
2025-04-05 12:00:08 +0200

    feat: configure headers to avoid caching files

--- src/controllers/audioFileController.js
+++ src/controllers/audioFileController.js
@@ -2,6 +2,14 @@ const path = require("path");
 
 function getAudioFile(req, res) {
   const { filename } = req.params;
+
+  // Configure headers to prevent caching
+  res.set({
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    Pragma: "no-cache",
+    Expires: "0",
+  });
+
   res.sendFile(path.join(__dirname, "../../audios", filename));
 }
 
